669ddded427b6eace622a20670fbac9f81a3f2b0
Shivam Sharma <shiv16085@gmail.com>
2020-07-02 23:47:56 +0530

    adding switch cases and scriplet tags

--- index.js
+++ index.js
@@ -12,14 +12,38 @@ app.get("/", function (req,res) {
 	var currentDay = today.getDay();
 	var day=""
 
-	if(currentDay ===6 || currentDay === 0){
-		day="weakend";
-	}else{
-		day="weakday";
+	switch(currentDay){
+		case 0:
+			day = "Sunday";
+			break;
+		case 1:
+			day = "Monday";
+			break;
+		case 2:
+			day = "Tuesday";
+			break;
+		case 3:
+			day = "Wednesday";
+			break;
+		case 4:
+			day = "Thursday";
+			break;
+		case 5:
+			day = "Friday";
+			break;
+		case 6:
+			day = "Saturday";
+			break;
+		default:
+				console.error("Error: current day is equal to "+currentDay);
+
 	}
 
+console.log(day);
 //sending the data to the ejs file
 	res.render("list",{kindOfDay:day});
+
+
 });
 
 app.listen(3000,function () {
