4fb9ee626d01fe5cf0826014a99c99dcb05f4464
Shivam Sharma <shiv16085@gmail.com>
2020-07-03 00:30:46 +0530

    finally todo v1 is completed,in this post data is handled from form and array of new list is made and sends again to same ejs file

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
+//importing modules in our file
 const express = require("express");
-
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // setting the ejs engine for handling ejs tempelatesa
 app.set('view engine','ejs');
 
+//using body-parser in our app
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+//declaring global variable
+//var item="" it is useless,so array make
+var items=["Buy food","Cook Food","Eat Food"];
+
 app.get("/", function (req,res) {
 	
 	var today = new Date();
-	var currentDay = today.getDay();
-	var day=""
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-	switch(currentDay){
-		case 0:
-			day = "Sunday";
-			break;
-		case 1:
-			day = "Monday";
-			break;
-		case 2:
-			day = "Tuesday";
-			break;
-		case 3:
-			day = "Wednesday";
-			break;
-		case 4:
-			day = "Thursday";
-			break;
-		case 5:
-			day = "Friday";
-			break;
-		case 6:
-			day = "Saturday";
-			break;
-		default:
-				console.error("Error: current day is equal to "+currentDay);
+	//console.log(today.toLocaleDateString("en-US", options));
+	var day = today.toLocaleDateString("en-US", options);
 
-	}
 
-console.log(day);
 //sending the data to the ejs file
-	res.render("list",{kindOfDay:day});
-
+	//res.render("list",{kindOfDay:day});
+//now above line code is changing bcz newListItem also wants to send 
+  res.render("list",{kindOfDay:day,newListItem:items})  //now it shows item not defined bcz it's in post method hence we will change it's scope
 
 });
+
+
+app.post("/",function (req,res) {
+
+  //var item = req.body.newItem;    ==>shows error bcz of scope
+  item = req.body.newItem; 
+
+  //add this below code at the end of the programing for finishing the list of items an array
+  items.push(item)
+
+//send new list items to the list.ejs  but it shows error bcz it does no send the data or 
+//newListItem bcs kindOfday is sending using get method,
+// res.render("list",{newListItem:item});
+//show that we will use redirect;
+res.redirect("/");
+})
 
 app.listen(3000,function () {
 	console.log("localhost 3000");
