465faa432f569ea3c98a79c449db1c6539ca3d5b
Shivam Sharma <shiv16085@gmail.com>
2020-07-03 15:08:49 +0530

    adding works list and printing on same web page (list.ejs) this is called TEMPELATING(text is dynamic)

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.use(express.static("public"));
 //var item="" it is useless,so array make
 var items=["Buy food","Cook Food","Eat Food"];
 
+//adding another list,work list
+var workItems=[];
+
 app.get("/", function (req,res) {
 	
 	var today = new Date();
@@ -30,25 +33,40 @@ app.get("/", function (req,res) {
 //sending the data to the ejs file
 	//res.render("list",{kindOfDay:day});
 //now above line code is changing bcz newListItem also wants to send 
-  res.render("list",{kindOfDay:day,newListItem:items})  //now it shows item not defined bcz it's in post method hence we will change it's scope
+  res.render("list",{listTitle:day,newListItems:items})  //now it shows item not defined bcz it's in post method hence we will change it's scope
 
 });
 
 
 app.post("/",function (req,res) {
 
-  //var item = req.body.newItem;    ==>shows error bcz of scope
+  //var item = req.body.newItem;    ==>shows error bcz of scope hence var declare global
   item = req.body.newItem; 
 
-  //add this below code at the end of the programing for finishing the list of items an array
-  items.push(item)
+  // //add this below code at the end of the programing for finishing the list of items an array
+  // items.push(item)
+
+//now two list are there so codintions are applied
+  if(req.body.button==="Work"){
+  	  	workItems.push(item);
+  	  	res.redirect("/work");
+  }else{
+    	items.push(item);
+    	res.redirect("/");
+  }
 
 //send new list items to the list.ejs  but it shows error bcz it does no send the data or 
 //newListItem bcs kindOfday is sending using get method,
 // res.render("list",{newListItem:item});
 //show that we will use redirect;
-res.redirect("/");
+//res.redirect("/");
 })
+
+
+//adding another route for work list or another list
+app.get("/work", function(req,res) {
+	res.render("list",{listTitle:"Work",newListItems:workItems});
+});
 
 app.listen(3000,function () {
 	console.log("localhost 3000");
